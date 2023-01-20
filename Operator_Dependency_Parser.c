a4b84a92c6254101f64e65d1373ca84873e561e8
fractalqualia <113944109+fractalqualia@users.noreply.github.com>
2023-01-20 04:55:23 +0000

    wrote some skeleton

---
+++ Operator_Dependency_Parser.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+// CREATING THREE STACKS
+int stack1[100], stack2[100], stack3[100];
+
+// CREATING THREE POINTERS
+int top1 = -1, top2 = -1, top3 = -1;
+
+// making precdence table 
+
